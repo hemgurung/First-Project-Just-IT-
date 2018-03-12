fe5dbd61296290cf87f864bca613457fba3fb1d9
hemgurung <32913468+hemgurung@users.noreply.github.com>
2018-03-12 23:19:55 +0000

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -4,40 +4,40 @@ var emailMB=0;
 var appMB=0;
 var musicMB=0;
 var videoMB=0;
+var totalMB=0;
 var totalGB=0;
 
 $(function() {
-    $(".webSliderC, .socialSliderC, .emailSliderC, .appSliderC, .musicSliderC, .videoSliderC").hide(); // Hide all sliders 
+    $(".webSliderC, .socialSliderC, .emailSliderC, .appSliderC, .musicSliderC, .videoSliderC").hide(); //Hide all sliders 
 });
 
 // WEB
 var slider = document.getElementById("webRange");
 var output = document.getElementById("webValue");
-output.innerHTML = slider.value+" minutes a day"; // Display the default slider value
+//output.innerHTML = slider.value+" minutes a day"; //Display the default slider value
 
-slider.oninput = function() { // Update the current slider value each time you drag the slider handle
-    output.innerHTML = this.value+" minutes a day"; 
+slider.oninput = function() { //Update the current slider value each time you drag the slider handle
+    output.innerHTML = this.value+" minutes a day (BROWSING, ONLINE GAMING)"; //Display slider value
     webMB=(this.value/60)*15*28;
     createChart();
 }
 
 $(function() { 
     $(".webBtn").click(function() {
-        $(this).toggleClass("active"); // Add 'active' class to clicked button
-        // Remove 'active' class from all other buttons
+        $(this).toggleClass("active"); //Add 'active' class to clicked button
+        //Remove 'active' class from all other buttons
         $(".socialBtn, .emailBtn, .appBtn, .musicBtn, .videoBtn").toggleClass("active",false); 
         $(".socialSliderC, .emailSliderC, .appSliderC, .musicSliderC, .videoSliderC").hide();   
-        $(".webSliderC").slideToggle(); // Jquery script to toggle slider
+        $(".webSliderC").slideToggle(); //Jquery script to toggle slider
     });
 });
 
 //SOCIAL
 var slider1 = document.getElementById("socialRange");
 var output1 = document.getElementById("socialValue");
-output1.innerHTML = slider1.value+" minutes a day"; 
 
 slider1.oninput = function() {
-    output1.innerHTML = this.value+" minutes a day"; 
+    output1.innerHTML = this.value+" minutes a day (FACEBOOK, TWITTER, VIBER, WHATSAPP, INSTAGRAM)"; 
     socialMB=(this.value/60)*20*28;
     createChart();
 }
@@ -53,11 +53,10 @@ $(function() {
 
 //EMAIL
 var slider2 = document.getElementById("emailRange");
-var output2 = document.getElementById("emailValue");
-output2.innerHTML = slider2.value+" emails a week"; 
+var output2 = document.getElementById("emailValue"); 
 
 slider2.oninput = function() {
-    output2.innerHTML = this.value+" emails a week";
+    output2.innerHTML = this.value+" emails a week (SENT/RECEIVED WITH ATTACHMENTS)";
     emailMB=this.value*0.3*4;
     createChart();
 }
@@ -74,10 +73,9 @@ $(function() {
 //APP
 var slider3 = document.getElementById("appRange");
 var output3 = document.getElementById("appValue");
-output3.innerHTML = slider3.value+" downloads a week"; 
  
 slider3.oninput = function() {
-    output3.innerHTML = this.value+" downloads a week";  
+    output3.innerHTML = this.value+" downloads a week (PLAYSTORE, ITUNES)";  
     appMB=this.value*12*4;
     createChart();
 }
@@ -94,10 +92,9 @@ $(function() {
 //MUSIC
 var slider4 = document.getElementById("musicRange");
 var output4 = document.getElementById("musicValue");
-output4.innerHTML = slider4.value+" minutes of streaming a day"; 
 
 slider4.oninput = function() {
-    output4.innerHTML = this.value+" minutes of streaming a day";  
+    output4.innerHTML = this.value+" minutes of streaming a day (SPOTIFY, SOUNDCLOUD)";  
     musicMB=(this.value/60)*80*28;
     createChart();
 }
@@ -114,10 +111,9 @@ $(function() {
 //VIDEO
 var slider5 = document.getElementById("videoRange");
 var output5 = document.getElementById("videoValue");
-output5.innerHTML = slider5.value+" minutes of streaming a day"; 
 
 slider5.oninput = function() {
-    output5.innerHTML = this.value+" minutes of streaming a day";  
+    output5.innerHTML = this.value+" minutes of streaming a day (YOUTUBE, NETFLIX)";  
     videoMB=(this.value/60)*350*28;
     createChart();
 }
@@ -135,8 +131,10 @@ var event1=document.getElementById("resultBtn");
 event1.addEventListener("click", calculate, false);
 
 function calculate() {
-    totalGB=(webMB+socialMB+musicMB+videoMB+emailMB+appMB)/1000;
+    totalMB=webMB+socialMB+musicMB+videoMB+emailMB+appMB;
+    totalGB=totalMB/1000;
     document.getElementById("dataResult").innerHTML=totalGB.toFixed(3)+" GB a month";
+    createPie();
 }
 
 //CHART
@@ -145,8 +143,11 @@ function createChart() {
 	theme: "light1", 
 	animationEnabled: true, 	
 	title: {
-		text: "Your Mobile Data Usage Per month in MB"
+		text: "Your mobile data usage per month"
 	},
+    axisY: {
+		title: "Data(MB)"
+    },
 	data: [ {
 		type: "column",
 		dataPoints: [
@@ -160,6 +161,31 @@ function createChart() {
 	}]
     });
 chart.render();
+}
+
+function createPie() {
+    var chart = new CanvasJS.Chart("pieContainer", {
+    theme: "light1",
+	animationEnabled: true,
+	title: {
+		text: "Your mobile data usage per month in percentage"
+	},
+	data: [{
+		type: "pie",
+		startAngle: 240,
+		yValueFormatString: "##0.00\"%\"",
+		indexLabel: "{label} {y}",
+		dataPoints: [
+			{y: (webMB/totalMB)*100, label: "Web"},
+			{y: (socialMB/totalMB)*100, label: "Social"},
+			{y: (emailMB/totalMB)*100, label: "Email"},
+			{y: (appMB/totalMB)*100, label: "App"},
+			{y: (musicMB/totalMB)*100, label: "Music"},
+            {y: (videoMB/totalMB)*100, label: "Video"}
+		]
+	}]
+});
+chart.render();  
 }
 
 
